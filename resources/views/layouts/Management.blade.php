2ba2b90073ca407c5e82eb1fee900f3fdf0b3285
parukia869 <mm.hh3813@gmail.com>
2024-08-20 18:50:08 +0900

    DB画面の基礎

---
+++ resources/views/layouts/Management.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<table>
+  <thead>
+    <tr>
+      <th>退場</th>
+      <th>ユーザー名</th>
+      <th>入場時間</th>
+      <th>退場時間</th>
+      <th>観戦試合</th>
+      <th>座席</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th><input type="radio"></th>
+      <td>name</td>
+      <td>time</td>
+      <td>time</td>
+      <td>game</td>
+      <td>seat</td>
+    </tr>
+    <tr>
+      <th><input type="radio"></th>
+      <td>name</td>
+      <td>time</td>
+      <td>time</td>
+      <td>game</td>
+      <td>seat</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
