0cd69801dd32ebbaaea6e373140d30e716043703
nanako1230 <arinochao1230@gmail.com>
2024-08-21 15:33:13 +0900

    topの文字だけ追加しました

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,65 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/DBscreen.css">
+</head>
+<div class="image">
+    <img src="resources/views/screen/image" alt="Toppage"/>
+</div>
+<div>
+    <h1 class="info">見たい試合を見ることが出来る</h1>
+    <p>relicketで簡単購入</p>
+    <h1>試合終了30分後料金が発生いたします（100円/10分）</h1>
+</div>
+<div>
+    <div>
+    <h1>本日の試合</h1>
+    <p>発売中◎　残り僅か△　売り切れ×</p>
+    </div>
+    <div>
+        <p>A高校 VS B高校</p>
+        <p>◎</p>
+    </div>
+    <div>
+        <p>C高校 VS D高校</p>
+        <p>△</p>
+    </div>
+    <div>
+        <p>E高校 VS F高校</p>
+        <p>×</p>
+    </div>
+    <button>翌日へ</button>
+</div>
+<div>
+    <div>
+        <img src="resources/views/screen/image" alt="Game1"/>
+        <p>A高校 VS B高校</p>
+        <p>空席あり◎</p>
+        <button>購入する</button>
+    </div>
+    <div>
+        <img src="resources/views/screen/image" alt="Game2"/>
+        <p>C高校 VS D高校</p>
+        <p>△</p>
+        <button>購入する</button>
+    </div>
+    <div>
+        <img src="resources/views/screen/image" alt="Game2"/>
+        <p>E高校 VS F高校</p>
+        <p>×</p>
+        <button>購入する</button>
+    </div>
+</div>
+<div>
+    <h1>お客様へのご案内</h1>
+    <p> 試合終了後、スムーズな会場運営にご協力をお願いいたします。</p>
+    <p>退出に関する重要事項は以下の通りです</p>
+    <p>試合終了後30分間は無料でお過ごしいただけます。
+        30分を超過した場合、10分ごとに100円の延長料金が発生いたします。
+        お時間に余裕を持ってのご退場にご協力いただけますと幸いです。ご理解とご協力をお願い申し上げます。</p>
+</div>
 
 
-    <!-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
-                font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
-                    掲示板へ移動
-                </a>
-            </div>
-        </div>
-    </div> -->
-    <div>
-        <img src="resources/views/screen/image" alt="Toppage"/>
-    </div>
-    <div>
-        <h1>見たい試合を見ることが出来る</h1>
-        <p>relicketで簡単購入</p>
-        <h1>試合終了30分後料金が発生いたします（100円/10分）</h1>
-    </div>
-    <div>
-        <div>
-        <h1>本日の試合</h1>
-        <p>発売中◎　残り僅か△　売り切れ×</p>
-        </div>
-        <div>
-            <p>A高校 VS B高校</p>
-            <p>◎</p>
-        </div>
-        <div>
-            <p>C高校 VS D高校</p>
-            <p>△</p>
-        </div>
-        <div>
-            <p>E高校 VS F高校</p>
-            <p>×</p>
-        </div>
-        <button>翌日へ</button>
-    </div>
-    <div>
-        <div>
-            <img src="resources/views/screen/image" alt="Game1"/>
-            <p>A高校 VS B高校</p>
-            <p>空席あり◎</p>
-            <button>購入する</button>
-        </div>
-        <div>
-            <img src="resources/views/screen/image" alt="Game2"/>
-            <p>C高校 VS D高校</p>
-            <p>△</p>
-            <button>購入する</button>
-        </div>
-        <div>
-            <img src="resources/views/screen/image" alt="Game2"/>
-            <p>E高校 VS F高校</p>
-            <p>×</p>
-            <button>購入する</button>
-        </div>
-    </div>
-    <div>
-        <h1>お客様へのご案内</h1>
-        <p> 試合終了後、スムーズな会場運営にご協力をお願いいたします。</p>
-        <p>退出に関する重要事項は以下の通りです</p>
-        <p>試合終了後30分間は無料でお過ごしいただけます。
-         30分を超過した場合、10分ごとに100円の延長料金が発生いたします。
-         お時間に余裕を持ってのご退場にご協力いただけますと幸いです。ご理解とご協力をお願い申し上げます。</p>
-    </div>
-</x-app-layout>
