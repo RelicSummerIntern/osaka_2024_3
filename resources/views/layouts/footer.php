6e90dae39d8c999c9487eda7d6295a0f26fd6756
nanako1230 <arinochao1230@gmail.com>
2024-08-21 14:04:20 +0900

    heaserをhtmlに変更しました

--- resources/views/layouts/footer.php
+++ resources/views/layouts/footer.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html><html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/DBscreen.css">
+</head>
+<body>
 <div class="">
     <div class="">
         <div class="">
@@ -32,3 +40,7 @@
         </div>
     </div>
 </div>
+
+</body>
+</html>
+
